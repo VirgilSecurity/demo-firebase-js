4c4a7adb7a012d02ef261ff362d36921d0d1cd07
Alexey Smirnov <alexey@smirnov.works>
2018-10-29 18:47:05 +0200

    remove useless component and extract ChatPrimitives

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,23 +1,83 @@
 import React from 'react';
 import firebase from 'firebase';
-import ChatWindow from '../components/ChatWindow';
 import ChatModel from '../models/ChatModel';
 import { IAppStore } from '../models/AppState';
+import Channels from '../components/Channels';
+import Messages from '../components/Messages';
+import MessageField from '../components/MessageField';
+import { LinkButton } from '../components/Primitives';
+import { IChannel } from '../models/ChannelModel';
+import { ChatContainer, Header, RightSide, ChatLayout, SideBar, BottomPrimaryButton, ChatWorkspace } from '../components/ChatPrimitives';
 
 export interface IChatPageProps {
-    store: IAppStore;
     model: ChatModel;
+    store: IAppStore;
 }
 
-class ChatPage extends React.Component<IChatPageProps> {
+export default class ChatPage extends React.Component<IChatPageProps> {
+    
+    componentWillUnmount() {
+        this.props.model.unsubscribe();
+    }
 
-    signOut = () => {
-        firebase.auth().signOut();
+    createChannel = async () => {
+        const receiver = prompt('receiver', '');
+        if (!receiver) return alert('Add receiver please');
+        try {
+            await this.props.model.channelsList.createChannel(receiver);
+        } catch (e) {
+            alert(e.message);
+        }
     };
 
+    sendMessage = async (message: string) => {
+        try {
+            await this.props.model.sendMessage(message)
+        } catch (e) {
+            alert(e);
+        }
+    }
+
+    selectChannel = (channelInfo: IChannel) => this.props.model.listenMessages(channelInfo);
+
     render() {
-        return <ChatWindow store={this.props.store} signOut={this.signOut} model={this.props.model} />;
+        if (this.props.store.error) alert(this.props.store.error);
+        return (
+            <ChatContainer>
+                <Header>
+                    <LinkButton color="white" href="https://virgilsecurity.com/" target="_blank">
+                        Virgilgram
+                    </LinkButton>
+                    <RightSide>
+                        {this.props.store.username}
+                        <LinkButton color="white" onClick={firebase.auth().signOut}>
+                            logout
+                        </LinkButton>
+                    </RightSide>
+                </Header>
+                <ChatLayout>
+                    <SideBar>
+                        <Channels
+                            onClick={this.selectChannel}
+                            username={this.props.store.username!}
+                            channels={this.props.store.channels}
+                        />
+                        <BottomPrimaryButton onClick={this.createChannel}>
+                            New Channel
+                        </BottomPrimaryButton>
+                    </SideBar>
+                    <ChatWorkspace>
+                        {this.props.store.currentChannel ? (
+                            <React.Fragment>
+                                <Messages messages={this.props.store.messages} />
+                                <MessageField handleSend={this.sendMessage} />
+                            </React.Fragment>
+                        ) : (
+                            'Select Channel First'
+                        )}
+                    </ChatWorkspace>
+                </ChatLayout>
+            </ChatContainer>
+        );
     }
 }
-
-export default ChatPage;
