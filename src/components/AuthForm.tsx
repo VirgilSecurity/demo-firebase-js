d712f4a1c7f3092c524ff6ea20c908d7b8967909
Alexey Smirnov <alexey@smirnov.works>
2018-11-16 12:12:46 +0200

    change username to email on form

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -92,7 +92,7 @@ export default class AuthForm extends React.Component<IAuthFormProps, IAuthFormS
     renderForm = ({ isValid }: FormikProps<IAuthFormValues>) => {
         return (
             <Form>
-                <Field name="username" render={this.renderEmailInput} />
+                <Field name="email" render={this.renderEmailInput} />
                 <Field name="password" render={this.renderPasswordInput} />
                 <Field name="brainkeyPassword" render={this.renderBrainKeyPasswordInput} />
                 {this.state.isLoading ? this.renderLoading() : this.renderButtons(isValid)}
